47313e6615f7db583df90bfb2dec47eb1a2a9691
gzb <1744998732@qq.com>
2018-09-24 22:47:05 +0800

    firstall

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+
+          <div class="footer__secondary__nav">
+            <span>Copyright © 2018 GZB</span>
+            <a href="http://guozebin.top">
+              About Me
+            </a>
+            <a href="#">
+              Program source code
+            </a>
+            <a href="#">
+              MyBlog
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
